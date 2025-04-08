d08ecac933463646a1597e7809293e6a82706170
Manuj <manujchadha7777@gmail.com>
2025-04-08 17:21:59 +0530

    Initial commit with frontend folder

---
+++ frontend/src/utils/constant.js
@@ -0,0 +1,5 @@
+export const USER_API_END_POINT="http://localhost:8080/api/users";
+export const JOB_API_END_POINT="http://localhost:8080/api/jobs";
+export const APPLICATION_API_END_POINT="http://localhost:8080/api/applications";
+export const COMPANY_API_END_POINT="http://localhost:8080/api/companies";
+export const AI_API_END_POINT="http://localhost:8080/api";
